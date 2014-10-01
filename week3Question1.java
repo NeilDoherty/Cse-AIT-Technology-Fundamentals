17d6c1cda2223f06317231dd6eb23ed57f23a808
unknown <A00226313@W301-A16.studentnet.ait.ie>
2014-10-01 16:24:08 +0100

    First commit

---
+++ week3Question1.java
@@ -0,0 +1,15 @@
+// Neil Doherty A00226313
+
+public class week3Question1 {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		if (a && !b)
+		{
+			System.out.println("TRUE");
+		}
+		else {
+			System.out.println("FALSE");
+		}
+	}
+}
