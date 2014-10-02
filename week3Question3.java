1b8b32386f917b235c0c834540dd1a1fe87fd5bc
Neil <neildoherty@live.ie>
2014-10-02 15:16:45 +0100

    Update week3Question3.java

--- week3Question3.java
+++ week3Question3.java
@@ -13,12 +13,15 @@ public class week3Question3 {
 		System.out.println("Enter a positive integer:");
 		String u1 = sc.next();
 		for (int x = 0 ; x < u1.length() ; x ++) {
-			int i1 = (int) u1.charAt(x);
+			int i1 = Character.getNumericValue(u1.charAt(x));
 			if ((i1 % 2) == 1) {
 				output = false;
 			}
 			else {
 				output = true;
+			}
+			if (output == false) {
+				x = u1.length()-1;
 			}
 		}
 		System.out.print("All digits in the number are even? ");
